1f796f600e6496d5d0adf8cd9d593152e60e40c3
David Stevenson <stevend@sandbox.dyndns.org>
2009-04-07 23:13:39 -0700

    cleanup sandbox

--- app/models/algorithm.rb
+++ app/models/algorithm.rb
@@ -4,6 +4,4 @@ class Algorithm < ActiveRecord::Base
  
   named_scope :best_first, { :order => "score / (games + 1) DESC" }
 
-  acts_as_runnable_code :classes => ["Entry"] #, :timeout => 1.0
-
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,9 +4,6 @@ class Entry < ActiveRecord::Base
 
   attr_reader :guessed
 
-  acts_as_wrapped_class :methods => [:word, :guesses, :all_words, :guess!]
-
-
   def word
     round.word.split("").collect{|l| guesses.include?(l) ? l : " "}.join
   end
