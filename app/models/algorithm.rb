2c08bd99a10bff62dc126923ed7158586e35d89f
David Stevenson <david@flouri.sh>
2009-04-11 14:09:55 -0700

    Added authorship

--- app/models/algorithm.rb
+++ app/models/algorithm.rb
@@ -1,8 +1,19 @@
 class Algorithm < ActiveRecord::Base
   attr_protected :games
   attr_protected :score
- 
+  attr_protected :password
+
   named_scope :best_first, { :order => "(CAST(score AS REAL) / (games + 1)) DESC" }
 
+  validates_presence_of :code
+  validates_presence_of :author
+  validates_presence_of :name
+  
+  validates_uniqueness_of :author
+  validates_uniqueness_of :name
+  validates_uniqueness_of :code
+
   acts_as_runnable_code :classes => ["Entry"]
+  
+  def current_password; ""; end
 end
